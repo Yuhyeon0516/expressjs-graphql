e32f8329b767cc44185e59eec50a198dee3b1548
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-29 20:57:21 +0900

    apollo3 server 구축

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const app = express();
-const port = 4000;
 const path = require("path");
-
+const port = 4000;
 const loadedFiles = loadFilesSync("**/*", {
   extensions: ["graphql"],
 });
 
 const loadedResolvers = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
-const schema = makeExecutableSchema({
-  typeDefs: loadedFiles,
-  resolvers: loadedResolvers,
-});
 
-app.use(
-  `/graphql`,
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+async function startApolloServer() {
+  const app = express();
 
-app.listen(port, () => {
-  console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
-});
+  const schema = makeExecutableSchema({
+    typeDefs: loadedFiles,
+    resolvers: loadedResolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(port, () => {
+    console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+  });
+}
+
+startApolloServer();
